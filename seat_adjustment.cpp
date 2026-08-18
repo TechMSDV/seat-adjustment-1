a1314fd07867ae6ede0e52f80e00d9dc7326eb51
TechMSDV <as001027583@techmahindra.com>
2026-08-18 10:05:49 +0530

    Update seat_adjustment.cpp

--- seat_adjustment.cpp
+++ seat_adjustment.cpp
@@ -113,7 +113,7 @@ void adjust_seat(const std::string& name, ordered_json current, ordered_json tar
   // ------------------ PROGRESS ------------------
 for (const std::string& key : update_order) {
     if (!current.contains(key) || !target.contains(key)) continue;
-    // if (key == "Headrest") continue;  
+    if (key == "Headrest") continue;  
 
     int current_val = current[key].get<int>();
     int target_val = target[key].get<int>();
